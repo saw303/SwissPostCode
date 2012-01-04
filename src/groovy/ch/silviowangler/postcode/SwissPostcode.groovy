8ec1c65dccd3613da7f6ac9e2b62746ca27ac870
Silvio Wangler <silvio.wangler@gmail.com>
2012-01-04 18:21:58 +0100

    Use AST transformation for equals, hashCode and toString

--- src/groovy/ch/silviowangler/postcode/SwissPostcode.groovy
+++ src/groovy/ch/silviowangler/postcode/SwissPostcode.groovy
@@ -1,35 +1,13 @@
 package ch.silviowangler.postcode
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@ToString
+@EqualsAndHashCode
 class SwissPostcode {
     String cityShortName
     String city
     String postcode
     String canton
-
-    String toString() {"{$postcode} {$city} (${canton})"}
-
-    @Override
-    boolean equals(o) {
-        if (this.is(o)) return true
-        if (getClass() != o.class) return false
-
-        SwissPostcode that = (SwissPostcode) o
-
-        if (canton != that.canton) return false
-        if (city != that.city) return false
-        if (cityShortName != that.cityShortName) return false
-        if (postcode != that.postcode) return false
-
-        return true
-    }
-
-    @Override
-    int hashCode() {
-        int result
-        result = cityShortName.hashCode()
-        result = 31 * result + city.hashCode()
-        result = 31 * result + postcode.hashCode()
-        result = 31 * result + canton.hashCode()
-        return result
-    }
 }
